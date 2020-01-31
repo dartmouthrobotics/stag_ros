585c1d0ccf40c31fc313d9b1e49c3b70020d4341
Sam Lensgraf <sam.lensgraf@gmail.com>
2020-01-31 18:53:57 +0000

    fix whitespace and some bugs

--- nodes/stagNodelet.cpp
+++ nodes/stagNodelet.cpp
@@ -11,6 +11,7 @@
 #include <ar_track_alvar_msgs/AlvarMarkers.h>
 #include <pluginlib/class_list_macros.h>
 #include <nodelet/nodelet.h>
+#include <boost/enable_shared_from_this.hpp>
 
 #include "Stag.h"
 #include "Marker.h"
@@ -50,6 +51,7 @@ public:
     int frame_number;
     int tag_id_type;
     boost::shared_ptr<void> this_ptr;
+    int _unused;
 
     ros::Subscriber image_subscriber; 
     ros::Subscriber camera_info_subscriber;
@@ -105,14 +107,6 @@ public:
             resultRotation,
             resultTranslation
         );
-        //cv::solvePnP(
-        //    objectPoints,
-        //    imagePoints,
-        //    cameraMatrix,
-        //    cv::Mat(),
-        //    resultRotation,
-        //    resultTranslation
-        //);
     }
 
     tf::StampedTransform cv_to_tf_transform(cv::Mat translation, cv::Mat rotation, std::string image_frame_id, std::string marker_frame_id, ros::Time stamp) {
@@ -217,9 +211,9 @@ public:
 
         auto output_to_marker_transform = camera_to_output_frame * camera_to_marker_transform;
 
-	transform_broadcaster_mutex.lock();
+        transform_broadcaster_mutex.lock();
         transform_broadcaster->sendTransform(camera_to_marker_transform);
-	transform_broadcaster_mutex.unlock();
+        transform_broadcaster_mutex.unlock();
 
         auto marker_pose_output_frame = tf_to_pose_stamped(
             output_to_marker_transform,
@@ -285,9 +279,6 @@ public:
 
         Stag stag(tag_id_type, 7, false);
 
-	//cv::Mat undistorted_image;
-	//cv::undistort(cv_ptr->image, undistorted_image, camera_matrix, distortion_coefficients);
-
         auto num_tags = stag.detectMarkers(cv_ptr->image);
 
         ar_track_alvar_msgs::AlvarMarkers markers_message;
@@ -304,9 +295,9 @@ public:
         markers_message.header.seq = image_message->header.seq;
         markers_message.header.frame_id = output_frame_id;
 
-	marker_message_mutex.lock();
+    	marker_message_mutex.lock();
         marker_message_publisher.publish(markers_message);
-	marker_message_mutex.unlock();
+        marker_message_mutex.unlock();
     }
 
     void camera_info_callback(const sensor_msgs::CameraInfoConstPtr& camera_info_msg) {
@@ -339,10 +330,10 @@ public:
         XmlRpc::XmlRpcValue marker_bundles_yaml;
         bool param_exists = private_node_handle.getParam("marker_bundles", marker_bundles_yaml);
 
-	if (!param_exists) {
+        if (!param_exists) {
             ROS_INFO("No marker bundle provided. Aborting marker bundle parsing.");
-	    return;
-	}
+            return;
+        }
 
         for (size_t bundle_index = 0; bundle_index < marker_bundles_yaml.size(); ++bundle_index) {
             MarkerBundle bundle_parsed;
@@ -367,15 +358,16 @@ public:
                         )
                     );
 
-                    marker_bundles.push_back(bundle_parsed);
                 }
             }
+
+            marker_bundles.push_back(bundle_parsed);
         }
     }
 
 
     void onInit() {
-	ROS_INFO("Initializing stag_ros");
+        ROS_INFO("Initializing stag_ros");
         distortion_coefficients = cv::Mat(5, 1, CV_32FC1);
 
         ros::NodeHandle& private_node_handle = getMTPrivateNodeHandle();
@@ -392,19 +384,18 @@ public:
         private_node_handle.getParam("output_frame_id", output_frame_id);
         private_node_handle.getParam("marker_message_topic", marker_message_topic);
 
-	ROS_INFO("Parsing individual marker sizes");
+        ROS_INFO("Parsing individual marker sizes");
         parse_marker_sizes(private_node_handle);
 
-	ROS_INFO("Parsing marker bundles");
+        ROS_INFO("Parsing marker bundles");
         parse_marker_bundles(private_node_handle);
 
         ros::SubscribeOptions opts;
-        this_ptr = boost::shared_ptr<void>(static_cast<void*>(this));
+        this_ptr = boost::shared_ptr<void>(static_cast<void*>(&(this->_unused)));
         boost::function<void (const boost::shared_ptr<const sensor_msgs::Image>& )> f2( boost::bind( &StagNodelet::image_callback, this, _1 ) );
         opts = opts.template create<sensor_msgs::Image>(camera_image_topic, 10, f2, this_ptr, NULL);
         opts.allow_concurrent_callbacks = true;
         opts.transport_hints = ros::TransportHints();
-
 
         camera_info_subscriber = private_node_handle.subscribe(camera_info_topic, 10, &StagNodelet::camera_info_callback, this);
 
@@ -414,8 +405,8 @@ public:
         transform_broadcaster = new tf::TransformBroadcaster();
         transform_listener = new tf::TransformListener();
 
-	std::string image_frame_id;
-	private_node_handle.getParam("image_frame_id", image_frame_id);
+        std::string image_frame_id;
+        private_node_handle.getParam("image_frame_id", image_frame_id);
 
         ROS_INFO("Waiting for transformation from image to output frame...");
         transform_listener->waitForTransform(output_frame_id, image_frame_id, ros::Time(0), ros::Duration(30.0));
@@ -431,10 +422,21 @@ public:
         }
 
         parse_marker_bundles(private_node_handle);
+        ROS_INFO_STREAM("Marker bundles:");
+        for (auto& bundle : marker_bundles) {
+            ROS_INFO_STREAM("    " << bundle.broadcasted_id << " with " << bundle.corner_world_locations.size() << " tags.");
+        }
+        ROS_INFO_STREAM("End marker bundles");
+
+        ROS_INFO_STREAM("Got marker sizes");
+        for (auto& key_value : marker_size_by_id) {
+            ROS_INFO_STREAM("marker id " << key_value.first << " size " << key_value.second);
+        }
+        ROS_INFO_STREAM("End marker sizes");
 
         image_subscriber = private_node_handle.subscribe(opts);
     }
 }; // class stag_ros
 
-PLUGINLIB_EXPORT_CLASS(stag_ros::StagNodelet, nodelet::Nodelet)
+PLUGINLIB_EXPORT_CLASS(stag_ros::StagNodelet, nodelet::Nodelet);
 } // namespace stag_ros
